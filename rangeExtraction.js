978ba27dbbd5bd66745b84a454e26c963319e5d5
JMarchesoni <jmarchesoni@gmail.com>
2017-10-01 21:19:46 -0400

    Fixed bugs and all tests pass

--- rangeExtraction.js
+++ rangeExtraction.js
@@ -1,23 +1,24 @@
-var rangeExtraction = function(numberList)
+var rangeExtraction = function(sortedSetArray)
 {
 	// Declare return variable
 	let output = "";
 
 	// Make a copy of the input array
-	let list = numberList.concat();
+	let list = sortedSetArray.concat();
 
 	// Initialize our variables to hold the first value and length of range.
 	// TODO: Change array-based code to discrete values
-	let first, last, length;
+	let first, last, length = 0;
 
 	// Declare variable to hold current separator. Setting it to an empty
-	// string allows us to skip a check later to see if the length of the
-	// consec array is equal to 1
+	// string allows us to skip a check later to see if length is equal to 1
 	let sep = "";
 
 	// Iterate over list
-	for (let value of list)
+	for (let count = 0; count <= list.length; count++)
 	{
+		// Set current value from array.
+		let value = list[count];
 
 		// If the current value is one greater than the last value
 		if (value === last + 1)
@@ -30,22 +31,19 @@ var rangeExtraction = function(numberList)
 		else
 		{
 			// Figure out whether we have two consecutive numbers or a true range
-			// (three or more) and assign the separator accordingly. We can't just
-			// check to see if it's equal to 2 because a length of one requries a
-			// a zero-length string for the separator.
-			sep = (length > 1 && length < 3) ? "," : "-";
+			// (three or more) and assign the separator accordingly.
+			if (length === 2) sep = ",";
+			if (length > 2) sep = "-";
 			
 			// We may need first and/or last to be empty strings depending on where
 			// we are in the loop: first run both are undefined, single non-
-			// consecutive number means last will be undefined, last run first will
-			// be undefined.
-			output += "," + (first || "") + sep + (last || "");
+			// consecutive number means last will be equal to first, last run first
+			// will be undefined.
+			output += (output ? "," : "") + (first || "") + sep;
+			output += (last !== first ? last || "" : "");
 
-			// Reinitialize first with the current value from list.
-			first = value;
-
-			// Reset last to undefined
-			last = void 0;
+			// Reinitialize first and last with the current value from list.
+			first = last = value;
 
 			// Set length to 1
 			length = 1;
